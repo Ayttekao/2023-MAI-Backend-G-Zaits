3a0d582fdaaa713b4a217aaeda1942e58306717e
Ayttekao <depish.huepish@yandex.ru>
2023-02-24 13:52:51 +0300

    Refactoring LRUCache class

--- lab_01/cache/LRUCache.py
+++ lab_01/cache/LRUCache.py
@@ -1,40 +1,61 @@
 import string
-from collections import deque
+
+from lab_01.cache.Node import Node
 
 
 class LRUCache:
     def __init__(self, capacity: int = 10) -> None:
-        self.__map___ = {}
-
         if capacity <= 0:
             raise ValueError("cache size must be greater than 0")
         else:
-            self.__deque__ = deque(maxlen=capacity)
-            self.__cache_size__ = capacity
+            self.capacity = capacity
+            self.cache = {}
+            self.head = Node()
+            self.tail = Node()
+            self.head.next = self.tail
+            self.tail.prev = self.head
 
     def get(self, key: str) -> str:
         result: string
 
-        try:
-            self.__deque__.remove(key)
-            self.__deque__.append(key)
-            result = self.__map___[key]
-        except ValueError:
+        if key in self.cache:
+            node = self.cache[key]
+            self._remove(node)
+            self._add(node)
+            result = node.value
+        else:
             result = ""
 
         return result
 
     def set(self, key: str, value: str) -> None:
-        if key not in self.__map___.keys():
-            if len(self.__deque__) == self.__cache_size__:
-                last = self.__deque__.popleft()
-                del self.__map___[last]
-        else:
-            self.remove(key)
+        if key in self.cache:
+            self._remove(self.cache[key])
 
-        self.__deque__.append(key)
-        self.__map___[key] = value
+        node = Node(key, value)
+        self.cache[key] = node
+        self._add(node)
+
+        if len(self.cache) > self.capacity:
+            node = self.head.next
+            self._remove(node)
+            del self.cache[node.key]
 
     def remove(self, key: str) -> None:
-        self.__deque__.remove(key)
-        del self.__map___[key]
+        if key in self.cache:
+            node = self.cache[key]
+            self._remove(node)
+            del self.cache[key]
+
+    def _remove(self, node):
+        prev = node.prev
+        next = node.next
+        prev.next = next
+        next.prev = prev
+
+    def _add(self, node):
+        prev = self.tail.prev
+        prev.next = node
+        node.prev = prev
+        node.next = self.tail
+        self.tail.prev = node
