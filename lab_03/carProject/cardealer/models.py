de1f779475cbd8427370e8ebd83ca4550d252a92
Ayttekao <depish.huepish@yandex.ru>
2023-04-10 20:01:32 +0300

    Implement lab 4

--- lab_03/carProject/cardealer/models.py
+++ lab_03/carProject/cardealer/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    make = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    year = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+
+
+class Sale(models.Model):
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
