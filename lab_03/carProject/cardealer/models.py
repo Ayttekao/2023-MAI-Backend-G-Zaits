7ddaa9a9f87e00a770d647992dfcaf37facc6cd9
Ayttekao <depish.huepish@yandex.ru>
2023-04-17 17:11:10 +0300

    Update models + migrations

--- lab_03/carProject/cardealer/models.py
+++ lab_03/carProject/cardealer/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 
 
+class Make(models.Model):
+    name = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'make'
+
+
 class Car(models.Model):
-    make = models.CharField(max_length=50)
+    make = models.ForeignKey(Make, on_delete=models.CASCADE)
     model = models.CharField(max_length=50)
     year = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        db_table = 'car'
 
 
 class Customer(models.Model):
@@ -13,9 +24,15 @@ class Customer(models.Model):
     email = models.EmailField()
     phone = models.CharField(max_length=20)
 
+    class Meta:
+        db_table = 'customer'
+
 
 class Sale(models.Model):
     car = models.ForeignKey(Car, on_delete=models.CASCADE)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     date = models.DateTimeField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        db_table = 'sale'
